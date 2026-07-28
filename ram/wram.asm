85912ba08158989663961e6dba1fee3768e5444f
CreamElDudJafar <jahern2398@gmail.com>
2026-07-27 21:23:52 -0500

    Fly To Route 4/10 Pokecenters Once You Enter Center
    https://github.com/pret/pokered/wiki/Allow-to-fly-to-any-map-and-from-any-map

--- ram/wram.asm
+++ ram/wram.asm
@@ -678,7 +678,7 @@ wRivalStarterBallSpriteIndex:: db
 NEXTU
 wFlyAnimUsingCoordList:: db
 ; $ff sentinel values at each end
-wFlyLocationsList:: ds NUM_CITY_MAPS + 2
+wFlyLocationsList:: ds NUM_FLY_LOCATIONS + 2
 
 NEXTU
 wWhichTownMapLocation:: db
@@ -2091,7 +2091,7 @@ wWalkBikeSurfState:: db
 
 wDumbByteToToggleStatusScreen:: db ; new
 
-wTownVisitedFlag:: flag_array NUM_CITY_MAPS
+wTownVisitedFlag:: flag_array NUM_FLY_LOCATIONS
 
 ; starts at 502
 wSafariSteps:: dw
