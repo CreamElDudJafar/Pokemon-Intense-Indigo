a0aacdb162973fa63096d4d083e7727e671548c3
CreamElDudJafar <jahern2398@gmail.com>
2026-08-25 10:53:02 -0500

    Add Some New Overworld NPCs Sprites To Match Gym Leaders

--- data/sprites/sprites.asm
+++ data/sprites/sprites.asm
@@ -67,6 +67,10 @@ SpriteSheetPointerTable:
 	overworld_sprite BrunoSprite, 12            ; SPRITE_BRUNO
 	overworld_sprite LoreleiSprite, 12          ; SPRITE_LORELEI
 	overworld_sprite SeelSprite, 12             ; SPRITE_SEEL
+	overworld_sprite BlaineSprite, 12           ; SPRITE_BLAINE
+	overworld_sprite ClairSprite, 12            ; SPRITE_CLAIR
+	overworld_sprite KrisSprite, 12             ; SPRITE_KRIS
+	overworld_sprite PryceSprite, 12            ; SPRITE_PRYCE
 	overworld_sprite PokeBallSprite, 4          ; SPRITE_POKE_BALL
 	overworld_sprite FossilSprite, 4            ; SPRITE_FOSSIL
 	overworld_sprite BoulderSprite, 4           ; SPRITE_BOULDER
